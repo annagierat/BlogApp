ead5ccaf6ef9be3ad8c86a23cb6acd13fe2e57ad
annagierat <anna.folwarska.88@gmail.com>
2018-10-28 12:17:06 +0100

    commit3

--- src/main/java/com/anna/gierat/blogApplication/controller/MainController.java
+++ src/main/java/com/anna/gierat/blogApplication/controller/MainController.java
@@ -2,13 +2,29 @@ package com.anna.gierat.blogApplication.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+        model.addAttribute("name","Anna");
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public String addPostPage(){
+        return "addPost";
+    }
+
+    @PostMapping("/addPost")
+    public String addPost(@RequestParam (value="title") String title,
+                          @RequestParam(value="content") String content){
+        System.out.println("Params: " + title + ", " + content);
         return "index";
     }
 
---
+++ src/main/java/com/anna/gierat/blogApplication/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.anna.gierat.blogApplication.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository {
+}
